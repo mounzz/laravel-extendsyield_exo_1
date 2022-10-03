e92e68a0cac6d7c2e34dc21cf4fc7dbc5f558380
Mounzz <senmounia@hotmail.com>
2022-10-03 11:36:56 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.index')
+@section('content')
+    <h1>mdr</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+
+    <nav class="bg-white shadow dark:bg-gray-800">
+        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+            <a href="/" class="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
+
+            <a href="/about" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">about</a>
+
+            <a href="/contact" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">contact</a>
+
+        </div>
+    </nav>
+</header>
+</body>
+</html>
